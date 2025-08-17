24cb6950305c2efd2826a8d73b102c0ed8b7ddcb
Shohan111369 <sarkershohan346@gmail.com>
2025-08-17 20:12:17 +0800

    Added ActivityCard and half Recentactivities

--- src/components/ReUseable/ActivityCard.jsx
+++ src/components/ReUseable/ActivityCard.jsx
@@ -3,7 +3,58 @@ import { Card, CardContent, Rating, Button } from "@mui/material";
 
 function ActivityCard({ name, location, date, rating, description, images }) {
   return (
-    <Card className="rounded-2xl shadow-md hover:shadow-lg transition bg-[#F8F8F8]"></Card>
+    <Card className="rounded-2xl shadow-md hover:shadow-lg transition bg-[#F8F8F8]">
+      <CardContent>
+        <div className="container mt-6  mb-6 ">
+          {/* Header */}
+          <div className="flex items-center justify-center lg:justify-start gap-3 mb-3">
+            <div className="bg-white">
+              <img src="Icon1.png" alt="" />
+            </div>
+            <div>
+              <h3 className="font-semibold">{name || "Unknown"}</h3>
+              <p className="text-gray-500 text-sm">{location || "N/A"}</p>
+            </div>
+          </div>
+
+          {/* Rating & Date */}
+          <div className="flex items-center justify-center lg:justify-start gap-2 mb-3">
+            <Rating value={rating || 0} readOnly size="small" />
+            <span className="text-gray-500 text-sm">{date || "No Date"}</span>
+          </div>
+
+          {/* Description */}
+          <div className="flex justify-center lg:justify-start w-full">
+            <p className="text-gray-600 text-sm mb-4 text-center lg:text-left max-w-[60%]">
+              {description || "No description available."}
+            </p>
+          </div>
+
+          {/* Images */}
+          <div className="flex justify-center lg:justify-start gap-6 mb-4">
+            {(images || []).map((img, i) => (
+              <img
+                key={i}
+                src={img}
+                alt="activity"
+                className="w-1/4 h-25 object-cover  "
+              />
+            ))}
+          </div>
+
+          {/* Button */}
+
+          <div className="flex justify-center lg:justify-start w-full">
+            <Button
+              variant="outlined"
+              className="rounded-full border-black normal-case"
+            >
+              Discover
+            </Button>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
   );
 }
 
--- src/components/RecentActivities.jsx
+++ src/components/RecentActivities.jsx
@@ -3,6 +3,42 @@ import { IconButton } from "@mui/material";
 import ActivityCard from "./ReUseable/ActivityCard";
 import ArrowButton from "./ReUseable/ArrowButton";
 
-function RecentActivities() {}
+function RecentActivities() {
+  const activities = [
+    {
+      name: "Leslie sakho",
+      location: "Canada, toronto",
+      date: "09/5/2025",
+      rating: 5,
+      description:
+        "The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.",
+      images: ["/last1.png", "/last2.png", "/last3.png"],
+    },
+    {
+      name: "Chris macari",
+      location: "Singapour",
+      date: "14/08/2025",
+      rating: 5,
+      description:
+        "The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.",
+      images: ["/last4.png", "/last5.png", "/last6.png"],
+    },
+    {
+      name: "Jojo alba",
+      location: "Kuala lumpur",
+      date: "28/05/2023",
+      rating: 5,
+      description:
+        "The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.",
+      images: ["/last7.png", "/last8.png", "/last9.png"],
+    },
+  ];
+
+  const handleNext = () => {
+    console.log("Right arrow clicked!");
+  };
+
+  return <section className="w-full py-10 px-6 "></section>;
+}
 
 export default RecentActivities;
